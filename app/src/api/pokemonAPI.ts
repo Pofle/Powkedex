64a77c7a199f220239ee064be1024f647c3256f6
PowFlew <pauline.fleury42@gmail.com>
2025-06-05 19:26:58 +0200

    Get list of all pokemons's names done.

--- app/src/api/pokemonAPI.ts
+++ app/src/api/pokemonAPI.ts
@@ -1,22 +1,27 @@
 /* export set tis type importable in the app : (import { PokemonListItem } from ...)*/
 export type PokemonDatas = {
-  id: number;  
-  name: string;
-  order: number;
+  /*id: number;  
+  order: number;*/
+  name: string;  
 };
 
 
-export async function fetchPokemonList(limit = 20): Promise<PokemonDatas[]> {
+export async function fetchPokemonList(limit : number): Promise<PokemonDatas[]> {
     /*Fetch result in constante */
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`);
     /* Transform result in json object */
   const data = await res.json();
 
-  return data.results.map((pokemon: { name: string; order: number; id: number }) => {
+  //return data.results.map((pokemon: { name: string }) => {
+  const pokemonDatas = data.results.map((pokemon: { name: string }) => {
     return {
-      id: pokemon.id,  
       name: pokemon.name,
-      order: pokemon.order
     };
   });
+
+  // FOR TESTING
+  console.log('3 premiers Pokémon :', pokemonDatas.slice(0, 3));
+
+   return pokemonDatas;
+
 }
--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,8 +1,14 @@
 import Head from "next/head";
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import PokemonCard from '../components/PokemonCard';
+import { fetchPokemonList, PokemonDatas } from '../api/pokemonApi';
+/*Here you specify that Home_component will receive a pokemonDatas prop, which is an array of Pokémon. */
+type HomeProps =  {
+  pokemonDatas: PokemonDatas[];
+}
 
-export default function Home() {
+export default function Home({pokemonDatas}: HomeProps) {
   return (
     <div>
       <Head>
@@ -23,8 +29,22 @@ export default function Home() {
           <p>Recherche</p>
         </div>
       </header>
+
+      <section className={styles.pokemonGrid}>
+          {pokemonDatas.map((pokemon) => (
+            <PokemonCard name={pokemon.name} />
+          ))}
+        </section>
     </main>
-    </div>  
-    
+    </div>     
   );
+  
+}export async function getStaticProps() {
+  const pokemonDatas = await fetchPokemonList(1302);
+
+  return {
+    props: {
+      pokemonDatas,
+    },
+  };
 }
